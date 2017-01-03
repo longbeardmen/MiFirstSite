8b2e1d05df28b8bff4b66abb8d1bb676a2c437f8
lokkirill <lokkirill@gmail.com>
2017-01-03 21:35:40 +0300

    no mailer

--- app/models/footbolist.rb
+++ app/models/footbolist.rb
@@ -1,8 +1,6 @@
 class Footbolist < ActiveRecord::Base
   mount_uploader :image, ImageUploader
   validates_presence_of :image
-  validates_presence_of :description
-  validates_presence_of :club
   validates_presence_of :second_name
   validates_presence_of :name
 end
